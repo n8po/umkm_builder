05bcfec098778631e81bd88e52c26d18ef54b371
Nathan <radityanatha20@gmail.com>
2026-01-31 22:44:19 +0800

    add content feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/header";
 import { DemoLayout } from "@/components/demo-layout";
 import { Footer } from "@/components/footer";
+import { ExpandableMapSection } from "@/components/expandable-map-section";
 
 export default function Home() {
   return (
     <>
       <Header />
       <DemoLayout />
+      <ExpandableMapSection />
       <Footer />
     </>
   );
